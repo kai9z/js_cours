1efec4380365fefeb0861655f888fb770c4a2edc
kai9z <and7kai9@gmail.com>
2020-01-18 22:10:40 +0200

    half of the road

--- classwork-2/main.js
+++ classwork-2/main.js
@@ -1,2 +1,27 @@
-//we learned some commands with git
-alert("Fuck");
+/*const oldString = "old 'string'";
+const commonString = 'common string';
+
+const yearOld = 1997;
+
+const es6String = `something here`; //Интерполяция строк
+
+const myName = "andrew";
+
+console.log(myName + ' born in ' + yearOld);
+console.log(`${myName} born in ${yearOld}`);
+
+console.log(`asd
+qwe`);
+
+const myNameUpperCase = myName.toUpperCase();
+
+console.log(myNameUpperCase);
+
+console.log("privet my code".toUpperCase());
+
+console.log("ALL LOWER CASE LETTERS".toLowerCase());
+*/
+//Slice - 2 numbers where to start and when go after
+const myName = "andrew";
+
+console.log(myName[0].toUpperCase() + myName.slice(1));
