c65b199d3308c8a4b55fc7fead5e4675b74f9b7b
kai9z <and7kai9@gmail.com>
2020-01-19 23:58:59 +0200

    P2 Finished with HW

--- classwork-2/main.js
+++ classwork-2/main.js
@@ -54,7 +54,7 @@ console.log ("supboi".repeat(3));
 STRINGS */
 
 // MATH
-
+/*
 console.log(10 + 20, 10 / 1, 20 * 5, 10 + 10 - 20);
 
 console.log('' +  10); // string + number == string
@@ -122,9 +122,81 @@ student['offers'] = ['facebook, google'];
  
 console.log(student);
 
-console.log();
+const easyWord = {
+    0:'e',
+    1:'a',
+    2:'s',
+    3:'y',
+    length: 4,
+}
 
+console.log(easyWord.length);
+console.log(easyWord[0]);
+*/
+// Homework
+/*
+ 1. Составить предложение из нижестоящих переменных:
+ "Сколько нужно программистов чтобы сделать проект ? 1, 25, команда"
+ */
 
+let numbers = [25, 1]
+let project = 'проект'
+let team = `команда`
+let howMuch = 'Сколько'
+let sentence = 'нужно программистов чтобы сделать проект ?'
 
+let task1 = (`HW task 1: "${howMuch} ${sentence} ${numbers[1]}, ${numbers[0]}, ${team}"`);
+console.log(task1);
 
+/*
+ 2. Составьте предложение из представленного массива
+ и выведите предложение в консоль;
+ "Так, когда Будда достиг Просветления, он обнаружил, что больше не ощущает себя мишенью.
+  Он не был больше ни телом, к которому рано или поздно ..."
+  // index +++
+ */
 
+let array = [
+  'Он',
+  'был больше ни телом, к которому рано или поздно',
+  'он обнаружил',
+  'не',
+  'Так, когда Будда достиг Просветления',
+  '...',
+  'что больше',
+  'ощущает себя мишенью',
+  'не',
+]
+
+let homeSentence = (`HW task 2: "${array[4]}, ${array[2]}, ${array[6]} ${array[3]} ${array[7]}.
+${array[0]} ${array[3]} ${array[1]} ${array[5]}"`);
+console.log(homeSentence);
+
+/*
+ 3. Добавьте свойста 4 новых свойства в объект используя
+    квадратные скобки и точку.
+ */
+
+let myObj = {}
+myObj.howitwork = [
+    'sup boi',
+    88005553535,
+
+];
+myObj.level = 25;
+myObj['3property'] = ['this hoes aint loyal'];
+myObj['DUDE'] = ['IM DONE'];
+console.log('HW task 3: ', myObj);
+
+/*
+ 5. Преобразуйте строку x,
+    в максимально удобно читаемый для программиста вид
+ */
+
+let frameworks = [4.7, 'Angular', '6Angular', 'React/Redux']
+
+const angular4 = frameworks[1] + Math.floor(frameworks[0]);
+const reactRedux = frameworks[frameworks.length - 1];
+
+const solutionHWT4 = `Google released new verison ${angular4} But real speed is ${reactRedux}`;
+console.log(solutionHWT4);
