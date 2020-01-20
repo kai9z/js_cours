4fb6ef42865fe78622dc4b3e9d17d2b73f1f51ad
kai9z <and7kai9@gmail.com>
2020-01-21 00:02:52 +0200

    p3 58 min done

---
+++ classwork-3/main.js
@@ -0,0 +1,48 @@
+// Модальные окна браузера Логические операции функции
+// Modal window blocks all scripts before you complete it
+
+// alert('Hello'); // ничего не возвращает
+// prompt('sup', 'dude')
+// confirm('yes no?');
+
+//const userNumber1 = prompt('Put first number here:');
+//const userNumber2 = prompt('Put second number here:');
+
+//alert(`'Your number is: ', ${+userNumber1 + +userNumber2}`);
+
+// Logical operations
+/*
+if(10 == 100) {
+    //ESLI
+    console.log('yeap there is TEN!')
+} else if (null != undefined) {
+    //INACHE ESLI
+    console.log('yeap you are here...')
+} else {
+    // INACHE
+    console.log('ruined')
+};
+*/
+/*
+const number1 = +prompt('put number:');
+if (number1 > 150) {
+    console.log('Tailand')
+} else if (number1 <= 100) {
+    if (number1 >= 0) {
+    console.log('Bali')
+    }
+} else {
+    console.log('Italy')
+}
+*/
+const testString = 'fuck';
+if (10 == 20) {
+    console.log('not an oprtion')
+} else if (testString == testString) {
+    if (testString.length < 2) {
+        console.log('you lost');
+    }
+    if (testString.length > 2){
+        console.log('you win')
+    }
+}
