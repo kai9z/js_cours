4bd3b87dc4474186ee39e1ce137fe5473df94716
kai9z <and7kai9@gmail.com>
2020-01-26 00:17:20 +0200

    4p1 done

--- classwork-4/main.js
+++ classwork-4/main.js
@@ -82,3 +82,72 @@ function myFunction(someArray) {
 super2(10, 20, 30, myFunction);
 
 */
+
+// LESSON 3
+/*
+var add2 = console.log(sum(10,20));
+
+// Function expreession - выражение
+const add = function newNameAdd (a, b) {
+    return a + b;
+}
+
+// Function declaration -выражение
+function sum(a,b) {
+    return a +  b;
+}
+*/
+/*
+* 1 - фаза инициализации переменных и функций
+* 2 - запуск программы
+*/
+
+// && - AND
+// || - or
+// ! - no
+/*
+const someUser = {
+    skill: {
+        programingLanguages: 'js',
+    },
+};
+
+const userName = someUser.name || 'defaultName0';
+
+console.log(someUser);
+console.log(userName);
+*/
+function argArray(mass) {
+    if (Array.isArray(mass) && (typeof mass[0]) == 'number' && isNaN(mass[0]) != true ) {
+        return 'win'
+    } else if ((typeof mass[0]) == 'string' || (typeof mass[0] == 'object')) {
+        return (mass[0])
+    } else {
+        return false;
+    }
+
+}
+const randomgArr = [NaN, 41, 2, 3]
+//console.log(randomgArr)
+console.log(argArray(randomgArr));
+
+if (isNaN(randomgArr[0]) == false){
+    console.log('hiii')
+} else {
+    'Nan???'
+}
+
+/*
+a = Array.isArray(randomgArr)
+console.log(a)
+b = typeof randomgArr[0] == 'number'
+console.log(b)
+c = isNaN(randomgArr[0]) != true
+console.log(c)
+
+if ((a && b) && c) {
+    console.log('hi')
+} else {
+    console.log ("nani")
+}
+*/
