197a949a539053f68faa1fa1222efbdc6c1511c5
kai9z <and7kai9@gmail.com>
2020-02-15 22:36:48 +0200

    1:15:16 one more task finished

--- classwork-5/main.js
+++ classwork-5/main.js
@@ -1,24 +1,60 @@
-let user = {
-    name: 'Andrew',
-    birthday: '23',
-    friends: [
-        { name: 'Seriy', birthday: '20.01.1996' },
-        { name: 'Toha', birthday: '08.2.1998' },
-        { name: 'danila', birthday: undefined },
-        { name: 'random', birthday: '03.12.1765' },
-    ],
-};
+// let user = {
+//     name: 'Andrew',
+//     birthday: '23',
+//     friends: [
+//         { name: 'Seriy', birthday: '20.01.1996' },
+//         { name: 'Toha', birthday: '08.2.1998' },
+//         { name: 'danila', birthday: undefined },
+//         { name: 'random', birthday: '03.12.1765' },
+//     ],
+// };
 
-function showUserFriends(userObj) {
-    const userName = user.name;
-    console.log(`${userName}'s friends:`)
-    const userFriends = Object.values(user.friends);
-    for (i in userFriends) {
-        friend = userFriends[i]
-        if (!friend.birthday) {
-            continue
-        }
-        console.log(`${friend.name} | Birthday: ${friend.birthday}`)
-    }       
+// function showUserFriends(userObj) {
+//     const userName = user.name;
+//     console.log(`${userName}'s friends:`)
+//     const userFriends = Object.values(user.friends);
+//     for (i in userFriends) {
+//         friend = userFriends[i]
+//         if (!friend.birthday) {
+//             continue
+//         }
+//         console.log(`${friend.name} | Birthday: ${friend.birthday}`)
+//     }       
+// }
+// showUserFriends(user);
+
+//
+
+let someObject = {
+    name: 'Test',
+    someNumber: 25,
+    someArray: [1, 2, 'test', 25, 50],
+    nextArray: [30, 40]
 }
-showUserFriends(user);
+
+const randomArray = [45, 1, 200, 'text']
+
+function showHightNumber(firstArg, secondArg) {
+    if (Array.isArray(firstArg)){
+        for (i in firstArg) {
+            if (firstArg[i] > secondArg) {
+                console.log(firstArg[i])
+            }
+        }
+    } else if (typeof(firstArg) == 'object') {
+        elem = Object.values(firstArg)
+        for (i in elem) {
+            if (Array.isArray(elem[i])) {
+                for (j in elem[i]) {
+                    if (elem[i][j] > secondArg){
+                        console.log(elem[i][j])
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+showHightNumber(someObject, 24);
